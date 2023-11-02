317b27fac83648fdd11c2be1f991908e7ad72443
Jamie Temple <jamie-temple@live.de>
2023-11-02 22:00:55 +0100

    feat(vscode): add java support

--- flake.nix
+++ flake.nix
@@ -71,6 +71,14 @@
             redhat.vscode-yaml
             tamasfe.even-better-toml
             yzhang.markdown-all-in-one
+
+            # Java
+            redhat.java
+            redhat.vscode-xml
+            redhat.vscode-yaml
+            vscjava.vscode-java-debug
+            vscjava.vscode-java-test
+            vscjava.vscode-maven
           ];
           mutableExtensionsDir = true;
           inherit (vscode) userSettings;
